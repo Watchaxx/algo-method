339b8499ef1a33058eed0794c565c8a595a70189
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2024-09-17 13:46:06 +0900

    Out.Flush();

--- 859/Program.cs
+++ 859/Program.cs
@@ -4,10 +4,8 @@ internal class Program
 {
     static void Main()
     {
-        var sw = new System.IO.StreamWriter( OpenStandardOutput() ) { AutoFlush = false };
-        SetOut( sw );
+        SetOut( new System.IO.StreamWriter( OpenStandardOutput() ) { AutoFlush = false } );
         // ↑が無いと不正解(時間切れ)になるヽ(`Д´)ﾉ
-        // SetOut( new ... ); と1行にまとめると何も出力されないヽ(`Д´)ﾉ
         int q = int.Parse( ReadLine() );
         int[] a = new int[20001];
 
@@ -19,7 +17,7 @@ internal class Program
         for( int i = 0; i < q; i++ ) {
             WriteLine( a[int.Parse( ReadLine() )] );
         }
-        sw.Dispose();
+        Out.Flush();
         return;
     }
 }
