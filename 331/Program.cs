b0dd751045b6346e1f6fd0ce99d6034a88b6f7fe
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2024-09-24 12:07:33 +0900

    ゴールドバッハ予想 (2)
    https://algo-method.com/tasks/331

---
+++ 331/Program.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( ReadLine() );
+        int o = 0;
+        bool[] p = new bool[n + 1].Select( x => x = true ).ToArray();
+
+        p[0] = false;
+        p[1] = false;
+        for( int i = 2; i <= n; i++ ) {
+            if( p[i] != true ) {
+                continue;
+            }
+            for( int j = i * 2; j <= n; j += i ) {
+                p[j] = false;
+            }
+        }
+        for( int i = 2; i <= n / 2; i++ ) {
+            if( p[i] == true && p[n - i] == true ) {
+                o++;
+            }
+        }
+        WriteLine( o );
+        return;
+    }
+}
